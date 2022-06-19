ef1b1bd56921e0f80b0d5c3330c0dcc6579b804a
NilsonPagnez <nilson.pagneto@gmail.com>
2022-06-19 16:16:36 -0300

    Todos os estudos até então

---
+++ Aula1/aula.js
@@ -0,0 +1,3 @@
+window.alert('Minha primeira mensagem!')
+window.confirm('Está gostando de JS?') //janela com botão ok e cancelar
+window.prompt('Qual é se nome')
---
+++ Aula11_condicionais/ex08_IF.js
@@ -0,0 +1,10 @@
+var vel = 40
+console.log(`A velocidade ${vel}KM/h`);
+if(vel > 60.5) {
+    console.log('Você foi multado')
+
+}
+    console.log('Dirija com segurança')
+
+
+
